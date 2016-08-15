58497e5167e2c54bccf9747efc359e1de9c0dcf0
Eugene <oniontastegarlic@gmail.com>
2016-08-15 17:30:40 +0900

    control fan

--- src/main.cpp
+++ src/main.cpp
@@ -28,18 +28,31 @@ void loop()
 //ROS Subscriber Test
 
 #include "std_msgs/UInt16.h"
-
+int fan01 = 7;
+int fan02 = 8;
+int fan03 = 9;
+int fan04 = 10;
+int val = 0;
 ros::NodeHandle nh;
 
 void led_cb(const std_msgs::UInt16 &cmd_msg){
-  digitalWrite(13, cmd_msg.data);
+  //digitalWrite(13, cmd_msg.data);
+  val = cmd_msg.data;
+  analogWrite(fan01, val);
+  analogWrite(fan02, val);
+  analogWrite(fan03, val);
+  analogWrite(fan04, val);
+  analogWrite(13, val);
 }
 
 ros::Subscriber<std_msgs::UInt16> sub("led", led_cb);
 
 void setup(){
   pinMode(13, OUTPUT);
-
+  pinMode(fan01, OUTPUT);
+  pinMode(fan02, OUTPUT);
+  pinMode(fan03, OUTPUT);
+  pinMode(fan04, OUTPUT);
   nh.initNode();
   nh.subscribe(sub);
 }
